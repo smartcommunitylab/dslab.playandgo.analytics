2da9a65bc61367ac913acc4ed36a48a591050a13
micnori <micnori@users.noreply.github.com>
2025-09-26 11:42:10 +0200

    Create save_csv.py

---
+++ save_csv.py
@@ -0,0 +1,16 @@
+from storage.storage_engine import FileStorage
+
+if __name__ == "__main__":    
+    storage_engine = FileStorage()
+    # nearest edges
+    size, df = storage_engine.load_dataframe("L", storage_engine.nearest_edges, "2025")
+    file_path = storage_engine.get_filename("L", storage_engine.nearest_edges, "2025")
+    storage_engine.save_csv(file_path, df)
+    # tracks info
+    size, df = storage_engine.load_dataframe("L", storage_engine.tracks_info, "2025")
+    file_path = storage_engine.get_filename("L", storage_engine.tracks_info, "2025")
+    storage_engine.save_csv(file_path, df)
+    # campaign tracks
+    #size, df = storage_engine.load_dataframe("L", storage_engine.campaign_tracks, "2025")
+    #file_path = storage_engine.get_filename("L", storage_engine.campaign_tracks, "2025")
+    #storage_engine.save_csv(file_path, df)
